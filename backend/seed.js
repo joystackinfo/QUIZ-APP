725fd69c40931d5dcd5de2a1066b83c35790461a
joystackinfo <joynelsoninfo@gmail.com>
2026-03-19 13:48:45 +0100

    Added seed.js

---
+++ backend/seed.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+const Question = require("./models/question.model");
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected for seeding"))
+  .catch(err => console.log(err));
