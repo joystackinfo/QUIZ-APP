b2ef413f556d589d1a1b8ea380720ab5cf1d116d
joystackinfo <joynelsoninfo@gmail.com>
2026-04-08 21:11:08 +0100

    added console

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use("/api/admin", adminRoutes); // Use admin routes
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, "../public")));
 
+console.log("Public folder path:", path.join(__dirname, "../public"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
